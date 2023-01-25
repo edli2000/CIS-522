6ec1c4d2410c02dcd3d1067fd0da94a9ee82c517
Edward Li <edli2000@seas.upenn.edu>
2023-01-24 19:50:01 -0500

    fixed unit test errors

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -2,20 +2,47 @@ import numpy as np
 
 
 class LinearRegression:
+    """
+    A linear regression model that uses a closed form solution to fit the model.
+    """
 
     w: np.ndarray
     b: float
 
     def __init__(self):
-        pass
+        self.w = 0
+        self.b = 0
 
-    def fit(self, X, y):
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fit the model for the given input using a closed form solution.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The input labels.
+
+        Returns:
+            np.ndarray: None.
+
+        """
         n = X.shape[0]
         X = np.append(X, np.ones((n, 1)), axis=1)  # Append column for bias
         y = y.reshape(n, 1)
         self.theta = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
+        w = self.theta[:, :-1]
+        b = self.theta[:, -1]
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+
+        """
         n = X.shape[0]
         X = np.append(X, np.ones((n, 1)), axis=1)
         return np.dot(X, self.theta)
@@ -29,14 +56,25 @@ class GradientDescentLinearRegression(LinearRegression):
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
-        self.m = 0
+        """
+        Fit the model for the given input using gradient descent.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The input labels.
+
+        Returns:
+            np.ndarray: None.
+
+        """
+        self.w = 0
         self.b = 0
         self.n = len(X)
         for i in range(epochs):
-            y_pred = self.m * X + self.b
+            y_pred = self.w * X + self.b
             dm = (-2 / self.n) * sum(X * (y - y_pred))
             db = (-1 / self.n) * sum(y - y_pred)
-            self.m = self.m - dm * lr
+            self.w = self.w - dm * lr
             self.b = self.b - db * lr
 
     def predict(self, X: np.ndarray) -> np.ndarray:
@@ -50,4 +88,4 @@ class GradientDescentLinearRegression(LinearRegression):
             np.ndarray: The predicted output.
 
         """
-        return self.m * X + self.b
+        return self.w * X + self.b
