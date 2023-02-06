85ad23652af44705383c04a1d21ba155e95dd152
Kyriezxc <90118603+Kyriezxc@users.noreply.github.com>
2023-02-06 17:18:48 -0500

    Update model_factory.py

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,6 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 200, output_dim, 2, torch.nn.LeakyReLU, torch.nn.xavier_normal_)
+    return MLP(
+        input_dim, 200, output_dim, 2, torch.nn.LeakyReLU, torch.nn.xavier_normal_
+    )
