b29d2059e7c74b6b5ad84a7b3d330511481fa803
Edward Li <edli2000@seas.upenn.edu>
2023-02-14 12:07:20 -0500

    submission

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,9 +6,8 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
-
     batch_size = 64
-    num_epochs = 3
+    num_epochs = 2
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
