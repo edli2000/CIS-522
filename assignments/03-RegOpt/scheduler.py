ed8be7f47823d9bb81b43ec0f0caa7e569716fed
Edward Li <edli2000@seas.upenn.edu>
2023-02-14 12:03:40 -0500

    submission

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,21 +4,34 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Class for custom learning rate scheduling
+    """
+
+    def __init__(self, optimizer, gamma=0.1, last_epoch=-1):
         """
         Create a new scheduler.
 
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
+        Arguments:
+            optimizer (torch.optim.Optimizer): model optimizer
+            gamma (float): Learning Rate decay factor
+            last_epoch (int): Last epoch index, defaulted to -1
+
+        Returns:
+            None
 
         """
-        # ... Your Code Here ...
+        self.gamma = gamma
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
+        """
+        Retrieves the list of learning rates
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        Arguments:
+            None
+
+        Returns:
+            None
+        """
+        return [i * self.gamma**self.last_epoch for i in self.base_lrs]
