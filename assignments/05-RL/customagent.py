848d3bd0ed308f3341250b289f71c024bb1b5f8e
Edward Li <edli2000@seas.upenn.edu>
2023-04-18 18:46:53 -0400

    test

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -2,6 +2,10 @@ import gymnasium as gym
 
 
 class Agent:
+    """
+    Class for the agent being trained to play the RL game
+    """
+
     def __init__(
         self, action_space: gym.spaces.Discrete, observation_space: gym.spaces.Box
     ):
@@ -9,6 +13,14 @@ class Agent:
         self.observation_space = observation_space
 
     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
+        """
+        Function for computing the actions of the agent
+
+        Args:
+            observation (gym.spaces.Box): state of environment
+        Returns:
+            gym.spaces.Discrete: a sampling of actions for the agent
+        """
         return self.action_space.sample()
 
     def learn(
@@ -18,4 +30,15 @@ class Agent:
         terminated: bool,
         truncated: bool,
     ) -> None:
+        """
+        Function for training the agent for the RL game
+
+        Args:
+            observation (gym.spaces.Box): state of environment
+            reward (float): amount of reward
+            terminated (bool): whether the game has terminated
+            truncated (bool): whether the game is truncated
+        Returns:
+            None
+        """
         pass
